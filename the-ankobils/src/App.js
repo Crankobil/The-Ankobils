149ab597bcd06338c2fefabbf0b3da5289c2cb45
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-12-21 12:03:35 +0000

    Initial Commit

---
+++ the-ankobils/src/App.js
@@ -0,0 +1,16 @@
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import MyJumbotron from './components/MyJumbotron';
+import MyNavbar from './components/MyNavbar'
+
+
+function App() {
+  return (<>
+    <MyNavbar/>
+    <MyJumbotron/>
+    </>
+  );
+}
+
+export default App;
---
+++ the-ankobils/src/components/MyJumbotron.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap"
+
+const MyJumbotron = () => {
+return (
+    <div className=" mb-4 bg-dark text-white">
+    <div className="container-fluid py-5 text-center">
+      <h1 className="display-5 fw-bold">Welcome to the Ankobil's Page!</h1>
+     
+    </div>
+  </div>
+)
+}
+
+export default MyJumbotron
