caa2d565b2f54f3ecf3e2e31741bb59a8fe17ae5
Crankobil <84187431+Crankobil@users.noreply.github.com>
2021-12-22 10:47:43 +0000

    Added CI false

--- the-ankobils/src/components/MyJumbotron.jsx
+++ the-ankobils/src/components/MyJumbotron.jsx
@@ -2,12 +2,12 @@ import { Container } from "react-bootstrap"
 
 const MyJumbotron = () => {
 return (
-    <div className=" mb-4 bg-dark text-white">
+    <Container className=" mb-4 bg-dark text-white" fluid>
     <div className="container-fluid py-5 text-center">
       <h1 className="display-5 fw-bold">Welcome to the Ankobil's Page!</h1>
      
     </div>
-  </div>
+  </Container>
 )
 }
 
